b06960e9646ba74c76093355fd7f3d0e8316f498
anshul4141 <anshulprajapati691@gmail.com>
2025-02-07 16:07:37 +0530

    done

---
+++ ORSProject-04/src/main/java/in/co/rays/bean/BaseBean.java
@@ -0,0 +1,54 @@
+package in.co.rays.bean;
+
+import java.sql.Timestamp;
+
+// BaseBean is parent Bean of all Bean classes
+public class BaseBean {
+
+	private Long id;
+	private String createdBy;
+	private String modifiedBy;
+	private Timestamp createdDatetime;
+	private Timestamp modifiedDatetime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Timestamp getCreatedDatetime() {
+		return createdDatetime;
+	}
+
+	public void setCreatedDatetime(Timestamp createdDatetime) {
+		this.createdDatetime = createdDatetime;
+	}
+
+	public Timestamp getModifiedDatetime() {
+		return modifiedDatetime;
+	}
+
+	public void setModifiedDatetime(Timestamp modifiedDatetime) {
+		this.modifiedDatetime = modifiedDatetime;
+	}
+
+}
